3bdab41fbf5f0ae982c7f06c4bd02ef11aac29d6
Elvin Efendi <elvin.efendiyev@gmail.com>
2015-07-01 01:39:51 +0200

    sqllite to postgresq

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150630220427) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "meta_events", force: :cascade do |t|
     t.string   "notes"
     t.datetime "starts_at"
